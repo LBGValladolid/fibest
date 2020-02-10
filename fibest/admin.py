41474c2b972e68c7feb0ef6c8e327c38beef7e1e
Juan <juanherruzo@gmail.com>
2020-02-10 20:34:11 +0100

    Eliminadas partes redundantes de la página de admin

--- fibest/admin.py
+++ fibest/admin.py
@@ -3,12 +3,7 @@ from django.contrib import admin
 from .models.company import Company, Magazine, Stand, Forum
 from .models.cv import CV
 
-# admin.site.register(Company)
 admin.site.register(CV)
-admin.site.register(Magazine)
-admin.site.register(Stand)
-
-admin.site.register(Forum)
 
 
 class ForumAdmin(admin.StackedInline):
