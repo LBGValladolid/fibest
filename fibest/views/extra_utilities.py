44e05544efebbe31e03d838b42eabf8a435cf2c2
Juan <juanherruzo@gmail.com>
2020-03-04 13:26:42 +0100

    Sector para descarga de base de datos en csv

--- fibest/views/extra_utilities.py
+++ fibest/views/extra_utilities.py
@@ -21,27 +21,37 @@ def contact_download(request):
         m = magazine.filter(company=c.id).first()
         s = stand.filter(company=c.id).first()
         f = forum.filter(company=c.id).first()
+        if c:
+            c_fields = c._meta.fields
+            tam = 21 - len(c_fields)
+            c_list = []
+            for f in c_fields:
+                c_list += getattr(m, f)
+            c_list = [None] * tam
+        else:
+            c_list = [None] * len(c._meta.fields)
+
         if m:
-            m_list = [m.publi_magazine,
-                      m.name_magazine, m.activity_magazine, m.search_magazine, m.year_magazine, m.workers_magazine,
-                      m.location_magazine, m.internships_magazine, m.beca_magazine, m.tfg_magazine, m.web_contact,
-                      m.email_contact, m.person_contact, m.phone_contact, m.postal_contact, m.twitter, m.facebook,
-                      m.instagram,
-                      m.youtube, m.linkedin, m.message_magazine]
+            m_fields = m._meta.fields
+            tam = 21 - len(m_fields)
+            m_list = []
+            for f in m_fields:
+                m_list += getattr(m, f)
+            m_list = [None] * tam
         else:
             m_list = [None] * 21
 
         if s:
-            s_list = [s.publi_stand,
-                      s.name_stand, s.activity_stand, s.search_stand, s.year_stand, s.workers_stand,
-                      s.location_stand, s.internships_stand, s.beca_stand, s.tfg_stand, s.web_contact,
-                      s.email_contact, s.person_contact, s.phone_contact, s.postal_contact, s.twitter, s.facebook,
-                      s.instagram,
-                      s.youtube, s.linkedin, s.message_stand]
+            s_fields = s._meta.fields
+            tam = 21 - len(s_fields)
+            s_list = []
+            for f in s_fields:
+                s_list += getattr(m, f)
+            s_list = [None] * tam
         else:
             s_list = [None] * 21
 
         writer.writerow(
-            [c.name, c.login, c.link, c.logo, c.stand_name, c.assembly_service, c.cvs_requested] + m_list + s_list)
+            c_list + m_list + s_list)
 
         return response
